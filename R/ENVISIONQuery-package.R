2ebc6ec92bd6a838205c74327576ff89696e8ce3
Chao-Jen Wong <cwon2@fhcrc.org>
2011-03-29 00:18:41 +0000

    Added the ENVISIONQuery package to the repository.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ENVISIONQuery@54126 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/ENVISIONQuery-package.R
@@ -0,0 +1,18 @@
+#' ENVISIONQuery.
+#'
+#' \tabular{ll}{
+#' Package: \tab ENVISIONQuery\cr
+#' Type: \tab Package\cr
+#' Version: \tab 1.0.5 \cr
+#' Date: \tab 2011-03-26 \cr
+#' License: \tab GPL-2\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#'
+#'
+#' @name ENVISIONQuery-package
+#' @aliases ENVISIONQuery-package
+#' @docType package
+#' @author Alex Lisovich, Roger Day
+#' @keywords package
+{}
