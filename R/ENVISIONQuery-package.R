a8b6572cf57df01a20bb5755cafcc551c3a5de39
Alex Lisovich <all67@pitt.edu>
2011-04-20 02:42:31 +0000

    ENVISIONQuery-package.R: the package creation date modified and version bumped to 1.1.2
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ENVISIONQuery@54973 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ENVISIONQuery-package.R
+++ R/ENVISIONQuery-package.R
@@ -3,7 +3,7 @@
 #' \tabular{ll}{
 #' Package: \tab ENVISIONQuery\cr
 #' Type: \tab Package\cr
-#' Version: \tab 1.1.0 \cr
+#' Version: \tab 1.1.2 \cr
 #' Date: \tab 2011-04-19 \cr
 #' License: \tab GPL-2\cr
 #' LazyLoad: \tab yes\cr
